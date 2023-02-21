a0a5b80c6f430d78fa62e9f9f2d7cc589c606782
Simon Muchemi <simonkenya846@gmail.com>
2023-02-22 00:57:42 +0300

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,13 +2,13 @@
 /**
  * print_sign - prints out the sign of a number
  * @n: Number to be checked
- * Return: 1 if number is positive, 0 if number is zero or -1 if number is negative
+ * Return: 1 for positive num, 0 for zero or -1 for negative num
  */
 int print_sign(int n)
 {
 	if (n < 0)
 	{
-		_putchar(43);
+		_putchar(45);
 		return (-1);
 	}
 	else if (n == 0)
@@ -18,7 +18,7 @@ int print_sign(int n)
 	}
 	else
 	{
-		_putchar(45);
+		_putchar(43);
 		return (1);
 	}
 }
