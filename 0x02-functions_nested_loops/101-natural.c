756d487e7b6062317bf6db8da8a3b532f75f78f7
Simon Muchemi <simonkenya846@gmail.com>
2023-02-22 01:40:29 +0300

    Task 101

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: zero if success
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 = 0 || i % 5 = 0)
+			printf("%d", i);
+	}
+	printf("\n");
+
+	return (0);
+}
