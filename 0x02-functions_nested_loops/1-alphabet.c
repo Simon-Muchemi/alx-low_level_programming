c093338b658661bb946f702adcb783b1406d2670
Simon Muchemi <simonkenya846@gmail.com>
2023-02-22 00:17:49 +0300

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - create the alphabets
+ * return: void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
