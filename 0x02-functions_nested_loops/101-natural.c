dcaad565bbe19591a0973c156fc33f299dfbe25c
Simon Muchemi <simonkenya846@gmail.com>
2023-02-22 01:45:14 +0300

    Task 101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,13 +7,14 @@
 int main(void)
 {
 	int i;
+	int sum = 0;
 
 	for (i = 0; i < 1024; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
-			printf("%d", i);
+			sum += i;
 	}
-	printf("\n");
+	printf("%d\n", sum);
 
 	return (0);
 }
