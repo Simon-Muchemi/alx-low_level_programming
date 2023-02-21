eb6187affd93f618d20e514a1fc344e84c09a0f4
Simon Muchemi <simonkenya846@gmail.com>
2023-02-21 15:31:22 +0300

    Task 0

---
+++ 0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints out '_putchar'
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char text = "_putchar";
+
+printf("%s", text);
+
+return (0);
+}
