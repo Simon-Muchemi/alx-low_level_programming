7d1cfcd81a83929ee58a033088346f59c6452158
Simon Muchemi <simonkenya846@gmail.com>
2023-02-24 00:00:57 +0300

    Task: 0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isupper - checks if a character is in uppercase
+ * Return: 1 if uppercase, 0 otherwise
+ */
+int int _isupper(int c)
+{
+	if (c > 64 && < 90)
+		return (1);
+	else
+		return (0);
+}
