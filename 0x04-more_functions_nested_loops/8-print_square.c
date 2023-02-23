7e6ce87db6ed9e15b8b093592889a2d4f167ae4b
Simon Muchemi <simonkenya846@gmail.com>
2023-02-24 01:24:52 +0300

    Task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,9 +14,9 @@ void print_square(int size)
 	{
 		int k, l;
 
-		for (k = 0; k <= size; k++)
+		for (k = 0; k < size; k++)
 		{
-			for (l = 0; l <= size; l++)
+			for (l = 0; l < size; l++)
 			{
 				_putchar('#');
 			}
