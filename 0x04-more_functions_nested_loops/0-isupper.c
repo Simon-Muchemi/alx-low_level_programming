5789c5afb4d87138790c3f7af7346de97b03ba0a
Simon Muchemi <simonkenya846@gmail.com>
2023-02-24 00:15:35 +0300

    Task: 0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,16 @@
 #include "main.h"
+
 /**
- * _isupper - checks if a character is in uppercase
- * @c: character param
- * Return: 1 if uppercase, 0 otherwise
+ * _isupper - checks is a character is uppercase
+ * @c: carrier integer variable
+ * Return: 1 if true, 0 if false
  */
-int int _isupper(int c)
+
+int _isupper(int c)
 {
-	if (c >= 'A' && <= 'Z')
+	if (c >= 65 && c <= 90)
+	{
 		return (1);
-	else
-		return (0);
+	}
+	return (0);
 }
