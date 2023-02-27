142b6321dcf540e28f9d39cd405b26f3b5e5dc3b
Simon Muchemi <simonkenya846@gmail.com>
2023-02-28 00:38:49 +0300

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _puts - prints a string in reverse followed by a new line
+ * print_rev - prints a string in reverse followed by a new line
  * @str: string param
  * Return: void
   */
