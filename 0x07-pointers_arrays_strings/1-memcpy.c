b8a2eee8d196d8143d2b2567ee1229802c4dcdab
Simon-Muchemi <simonkenya8465@gmail.com>
2023-03-06 16:03:08 +0300

    Task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *_memcpy - copys specified bytes of memory area
+ * @dest: pointer to first string parameter
+ * @src: pointer to string parameter 2
+ * Return: dest string
+
+char *_memcpy(char *dest, char *src, unasigned int n)
+{
+	unasigned int j;
+
+	for (j = 0; j < n; j++)
+	{
+		dest[j] = src[j];
+	}
+	return (dest);
+}
