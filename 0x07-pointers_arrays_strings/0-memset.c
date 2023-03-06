311448e3992f9b207a4e6f4292efb0ff002004d1
Simon-Muchemi <simonkenya8465@gmail.com>
2023-03-06 15:53:58 +0300

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,15 +4,15 @@
  * @s: pointer to put the constant
  * @b: constant
  * @n: max bytes to use
- * Return: pointer to s
+ * Return: s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int j;
+	unsigned int i;
 
-	for (j = 0; n > 0; j++, n--)
+	for (i = 0; n > 0; i++, n--)
 	{
-		s[j] = b;
+		s[i] = b;
 	}
 
 	return (s);
