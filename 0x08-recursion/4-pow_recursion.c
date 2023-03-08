ed22a2499b396dc2f4eaa71de0a72721aa0accbf
Simon-Muchemi <simonkenya846@gmail.com>
2023-03-08 17:57:41 +0300

    Task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,15 +9,14 @@
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-       	{
-	       	return -1;
+	{
+		return (-1);
 	}
 	else if (y == 0)
-    	{
-        	return 1;
-    	}
+	{
+		return (1);
+	}
+	y--;
 
-    	y--;
-
-    	return (x * _pow_recursion(x, y));
+	return (x * _pow_recursion(x, y));
 }
